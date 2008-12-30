a550e58c7cd8994448e159437fe1fdfd4c2bce42
Aaron Feng <aaron.feng@gmail.com>
2008-12-30 17:49:38 -0500

    refactored the winforms stuff to be less scriptish by encapsulating in a class

--- Forms.fs
+++ Forms.fs
@@ -3,40 +3,41 @@
 open System
 open System.Drawing
 open System.Windows.Forms
-
-let directory_tree = new TreeView(Dock = DockStyle.Fill)
-
-let database_tree = new TreeView(Dock = DockStyle.Fill)
-
-let vertical_split = new SplitContainer()
-vertical_split.Orientation <- Orientation.Vertical
-vertical_split.Dock <- DockStyle.Fill
-vertical_split.BorderStyle <- BorderStyle.Fixed3D
-vertical_split.SplitterIncrement <- 1
-vertical_split.SplitterDistance <- 100
-vertical_split.BackColor <- Color.Aqua
-vertical_split.Panel1.Controls.Add(directory_tree)
-vertical_split.Panel2.Controls.Add(database_tree)
-
-let horizonal_split = new SplitContainer()
-horizonal_split.Orientation <- Orientation.Horizontal
-horizonal_split.Dock <- DockStyle.Fill
-horizonal_split.SplitterIncrement <- 1
-horizonal_split.SplitterDistance <- 400
-horizonal_split.Panel1.Controls.Add(vertical_split)
-horizonal_split.BackColor <- Color.Blue
-
-let master_container = new ToolStripContainer()
-master_container.Dock <- DockStyle.Fill
-master_container.ContentPanel.Controls.Add(horizonal_split)
       
+type MainForm() as self = 
+    class 
+        inherit Form(Text = "F# SchemaRunner", Width = 786, Height = 560, MinimumSize = new Size(500, 300)) 
+        let directory_tree = new TreeView(Dock = DockStyle.Fill)
+        let database_tree = new TreeView(Dock = DockStyle.Fill)
+
+        let vertical_split as v = new SplitContainer()
+        do v.Orientation <- Orientation.Vertical
+        do v.Dock <- DockStyle.Fill
+        do v.BorderStyle <- BorderStyle.Fixed3D
+        do v.SplitterIncrement <- 1
+        do v.SplitterDistance <- 100
+        do v.BackColor <- Color.Aqua
+        do v.Panel1.Controls.Add(directory_tree)
+        do v.Panel2.Controls.Add(database_tree)
+
+        let horizonal_split as h = new SplitContainer()
+        do h.Orientation <- Orientation.Horizontal
+        do h.Dock <- DockStyle.Fill
+        do h.SplitterIncrement <- 1
+        do h.SplitterDistance <- 400
+        do h.Panel1.Controls.Add(vertical_split)
+        do h.BackColor <- Color.Blue
+
+        let master_container as m = new ToolStripContainer()
+        do m.Dock <- DockStyle.Fill
+        do m.ContentPanel.Controls.Add(horizonal_split)
         
-let form = new Form()
-form.Text <- "F# SchemaRunner"
-form.Height <- 560
-form.Width <- 786
-form.MinimumSize <- new Size(500, 300)
-form.Controls.Add(master_container)
+        do self.Controls.Add(master_container)
+        do self.Show()
+        
+    end 
+    
+let form = new MainForm()
 
 #if INTERACTIVE
 form.Show()
