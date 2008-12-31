97698688d37d2734f8c48a0c0edd24740d76c7df
Aaron Feng <aaron.feng@gmail.com>
2008-12-31 06:49:56 -0500

    refactored main form to use inline constructor and added a Tree class

--- Forms.fs
+++ Forms.fs
@@ -1,40 +1,41 @@
 ﻿#light
+module LRF.SchemaRunner.Forms
 
 open System
 open System.Drawing
 open System.Windows.Forms
+
+type Tree() as self =
+    class
+        inherit TreeView(Dock = DockStyle.Fill)
+        do self.Nodes.Add("test") |> ignore
+end
       
 type MainForm() as self = 
     class 
         inherit Form(Text = "F# SchemaRunner", Width = 786, Height = 560, MinimumSize = new Size(500, 300)) 
-        let directory_tree = new TreeView(Dock = DockStyle.Fill)
-        let database_tree = new TreeView(Dock = DockStyle.Fill)
+        let directory_tree = new Tree()
+        let database_tree = new Tree()
 
-        let vertical_split as v = new SplitContainer()
-        do v.Orientation <- Orientation.Vertical
-        do v.Dock <- DockStyle.Fill
-        do v.BorderStyle <- BorderStyle.Fixed3D
-        do v.SplitterIncrement <- 1
-        do v.SplitterDistance <- 100
-        do v.BackColor <- Color.Aqua
+        let vertical_split as v = new SplitContainer(Orientation = Orientation.Vertical,
+                                                     Dock = DockStyle.Fill,
+                                                     BorderStyle = BorderStyle.Fixed3D,
+                                                     SplitterIncrement = 1,
+                                                     SplitterDistance = 100)
         do v.Panel1.Controls.Add(directory_tree)
         do v.Panel2.Controls.Add(database_tree)
 
-        let horizonal_split as h = new SplitContainer()
-        do h.Orientation <- Orientation.Horizontal
-        do h.Dock <- DockStyle.Fill
-        do h.SplitterIncrement <- 1
-        do h.SplitterDistance <- 400
+        let horizonal_split as h = new SplitContainer(Orientation = Orientation.Horizontal,
+                                                      Dock = DockStyle.Fill,
+                                                      SplitterIncrement = 1,
+                                                      SplitterDistance = 400)
         do h.Panel1.Controls.Add(vertical_split)
-        do h.BackColor <- Color.Blue
 
-        let master_container as m = new ToolStripContainer()
-        do m.Dock <- DockStyle.Fill
+        let master_container as m = new ToolStripContainer(Dock = DockStyle.Fill)
         do m.ContentPanel.Controls.Add(horizonal_split)
         
         do self.Controls.Add(master_container)
         do self.Show()
-        
     end 
     
 let form = new MainForm()
